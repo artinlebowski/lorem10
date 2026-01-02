c82eac7ea49df754249194289aebbada27bd9b1c
website <brolebowski85@gmail.com>
2026-01-01 23:41:46 -0800

    Add files via upload

---
+++ context/languageContext.js
@@ -0,0 +1,18 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+const LanguageContext = createContext();
+
+export function LanguageProvider({ children }) {
+  const [lang, setLang] = useState("fa");
+
+  return (
+    <LanguageContext.Provider value={{ lang, setLang }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+}
+
+export function useLanguage() {
+  return useContext(LanguageContext);
+}
